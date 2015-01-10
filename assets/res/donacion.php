21ab5bbdc5c7ca9bc0617c11aaf9c42906ec7c58
Hector Romero <hector.romero@fdvsolutions.com>
2015-01-10 14:27:00 -0300

    Added missing php files

---
+++ assets/res/donacion.php
@@ -0,0 +1,314 @@
+<h2>Hola Amigos!</h2>
+<br>
+<p>Como muchos esperaban, por fin vamos a habilitar las donaciones!</p>
+<p>Las mismas se usarán para ayudar a mantener el server, solventando gastos, para poder seguir brindandoles la mejor calidad de juego posible.</p>
+<p>A cambio de sus aportes, serán ascendidos a alguno de los rangos VIP y obtendran ciertos beneficios extra en el juego, los  que se detallarán a continuación.</p>
+<br>
+<h3>Rangos y Beneficios:</h3>
+<p>Por el momento, solo se podra donar para dos rangos VIP:<br>
+    - <strong>VIP Hierro</strong><br/>
+    - <strong>VIP Diamante</strong>
+</p>
+<p>Ambos VIPs tienen todos los beneficios que se alcanzan hasta el rango <a href="http://foro.diquemc.com/noticias-e-informacion/rangos/">diamante</a> mas los siguientes:
+</p>
+<p>
+    * Acceso a la tienda vip. <br>
+    * Mesa de crafteo portatil. (/craft). <br>
+    * TP a la ubicacion anterior (/back). <br>
+    * Peticion de TP hacia tu ubicacion actual (/tpahere) <br>
+    * Nombre con color (/nick). <br>
+    * Chat con color (ver kit color).<br>
+    * Carteles con color (ver kit color).<br>
+    * Kit color (/kit color). <br>
+    * usar cualquier bloque como sombrero (/hat). <br>
+    * Preservacion de la experiencia al morir. <br>
+    * Reparacion de herramientas y armaduras (/repair). <br>
+    * TP a residencia (/res settp y /res tp {nombre de residencia} <br>
+</p>
+
+<br/>
+<br/>
+<table class="vipTable">
+    <thead>
+        <tr>
+            <td>
+                <strong>
+                    <h2 class="center">Vip Hierro</h2>
+                </strong>
+            </td>
+            <!--<td>&nbsp;&nbsp;&nbsp;</td>-->
+            <td>
+                <strong>
+                    <h2 class="center">Vip Diamante</h2>
+                </strong>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="assets/vipHierro.png" alt="" height="100px"/>
+            </td>
+            <!--<td>&nbsp;&nbsp;&nbsp;</td>-->
+            <td>
+                <img src="assets/vipDiamante.png" alt="" height="100px"/>
+            </td>
+        </tr>
+    </thead>
+    <tbody>
+
+    <tr>
+        <td>
+            <p>
+                * Residencias :   15 (150x150) <br>
+                * Home's (Casas) :  25 (/sethome) <br>
+                * Kit hierro: Armadura completa y Espada (/kit hierro)<br>
+                * Kit regalo: (/kit hierroRegalo)<br>
+                &nbsp;- Pico de hierro encantado.<br>
+                &nbsp;- Pala de hierro.<br>
+                &nbsp;- Hacha de hierro.<br>
+                &nbsp;- Azada de hierro.<br>
+                &nbsp;- Botellas de experiencia. (32)<br>
+                &nbsp;- Carne cocida. (32)<br>
+                &nbsp;- Bloque Musical.<br>
+            </p>
+
+        </td>
+        <!--<td></td>-->
+        <td>
+            <p>
+                * Residencias : 20 (180x180) <br>
+                * Home's (Casas) : 35 (/sethome) <br>
+                * Kit diamante <br>
+                * Podes usar particulas como sombrero (/hats)<br>
+                * Kit diamante: Armadura completa y Espada (/kit diamante)<br>
+                * Kit regalo: (/kit diamanteRegalo)<br>
+                &nbsp;- Pico de diamante encantado.<br>
+                &nbsp;- Pala de diamante.<br>
+                &nbsp;- Hacha de diamante.<br>
+                &nbsp;- Azada de diamante.<br>
+                &nbsp;- Botellas de experiencia. (64)<br>
+                &nbsp;- Carne cocida. (64)<br>
+                &nbsp;- Toca Discos.<br>
+                &nbsp;- Disco.<br>
+                - Ademas:<br>
+                * Ciudad de 300x300 :<br>
+                &nbsp;- Con warp.<br>
+                &nbsp;- Con fly para el dueño y 5 jugadores mas.<br>
+                &nbsp;- Sala con nombre de la ciudad en nuestro TS3 <br>
+
+            </p>
+        </td>
+    </tr>
+
+    <tr>
+
+    </tr>
+    <tr>
+        <td> AR$60,00  / u$s 7,00 </td>
+        <td> AR$110,00  / u$s 13,00 </td>
+    </tr>
+
+    <tr>
+        <td style="padding-left: 3em;box-sizing: border-box">
+            <a href="https://www.mercadopago.com/mla/checkout/pay?pref_id=940152-d265ecf8-5d08-4485-9b1b-391554c62d7e" name="MP-payButton" class="blue-ar-s-rn-arall">Mercado Pago</a>
+            <script type="text/javascript">
+            (function(){function $MPBR_load(){window.$MPBR_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = ("https:"==document.location.protocol?"https://www.mercadopago.com/org-img/jsapi/mptools/buttons/":"http://mp-tools.mlstatic.com/buttons/")+"render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPBR_loaded = true;})();}window.$MPBR_loaded !== true ? $MPBR_load(): null;})();
+            </script>
+        </td>
+
+        <td style="padding-left: 3em;box-sizing: border-box">
+            <a href="https://www.mercadopago.com/mla/checkout/pay?pref_id=940152-30baad7c-fe3b-4700-8930-520614078423" name="MP-payButton" class="blue-ar-s-rn-arall">Mercado Pago</a>
+            <script type="text/javascript">
+            (function(){function $MPBR_load(){window.$MPBR_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = ("https:"==document.location.protocol?"https://www.mercadopago.com/org-img/jsapi/mptools/buttons/":"http://mp-tools.mlstatic.com/buttons/")+"render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPBR_loaded = true;})();}window.$MPBR_loaded !== true ? $MPBR_load() : null;})();
+            </script>
+
+        </td>
+    </tr>
+
+
+    <tr>
+        <td style="padding-left: 3.5em;box-sizing: border-box">
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="4QSZ25LQBQJZ2">
+            <input type="image" src="http://diquemc.com/images/boton-paypal.png" border="0" name="submit" alt="PayPal. La forma rápida y segura de pagar en Internet.">
+            <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+            </form>
+
+        </td>
+        <td style="padding-left: 3.5em;box-sizing: border-box">
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="EH6DFPLC53NNE">
+            <input type="image" src="http://diquemc.com/images/boton-paypal.png" border="0" name="submit" alt="PayPal. La forma rápida y segura de pagar en Internet.">
+            <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+            </form>
+
+        </td>
+    </tr>
+    </tbody>
+
+</table>
+
+<br/><br/>
+<h2>Warps:</h2>
+<h3>¿Que es un warp?</h3>
+<p>Es un acceso directo a un lugar. Simplemente con poner /warp + nombre del warp puedes acceder.</p>
+<h3>Beneficios:</h3>
+<p>
+    Te ahorras dos /sethome<br/>
+    Puedes hacerte una mini cuidad,tiendas, super casas,
+    Parcelas para vender(¡mejora tu economia!) o simplemente un lugar exclusivamente TUYO donde tus amigos puedan visitarte facil y practicamente!</p>
+
+ <p>Llevate 2 warps a solo $40,00 (¡Este  warp será puesto como acceso directo en Nuestra Ciudad Dique!)</p>
+<div>
+<table class="warpTable" style="text-align: center;display:block;margin-left: 10em;">
+<tbody>
+    <tr>
+        <td>
+            <img src="assets/warp.png" alt="" height="100px"/>
+        </td>
+    </tr>
+
+    <tr>
+        <td> AR$40,00  / u$s 5,00 </td>
+    </tr>
+
+    <tr>
+        <td style="/*padding-left: 3em;box-sizing: border-box*/">
+            <a href="https://www.mercadopago.com/mla/checkout/pay?pref_id=940152-27349d82-6ad1-41cf-887f-06a1a6befc35" name="MP-payButton" class="blue-ar-s-rn-arall">Mercado Pago</a>
+            <script type="text/javascript">
+            (function(){function $MPBR_load(){window.$MPBR_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = ("https:"==document.location.protocol?"https://www.mercadopago.com/org-img/jsapi/mptools/buttons/":"http://mp-tools.mlstatic.com/buttons/")+"render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPBR_loaded = true;})();}window.$MPBR_loaded !== true ? $MPBR_load(): null;})();
+            </script>
+        </td>
+
+    </tr>
+
+    <tr>
+        <td style="/*padding-left: 3.5em;box-sizing: border-box*/">
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="X6H4HZPSRUJXG">
+            <input type="image" src="http://diquemc.com/images/boton-paypal.png" border="0" name="submit" alt="PayPal. La forma rápida y segura de pagar en Internet.">
+            <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+            </form>
+
+        </td>
+    </tr>
+    </tbody>
+
+</table>
+</div>
+
+<br/><br/>
+
+<h2>Skins:</h2>
+<p>¿Estás cansado de ser un Steve? ¿Queres reflejar un poco de tu personalidad a través de un skin?</p>
+<p>
+    Hemos agregado la posibilidad de que tengas el skin que desees en nuestro server, asi ya te podran ver como alguien único! Ya que el adquirir una cuenta premium para minecraft no esta al alcance de todos.
+
+</p>
+<h3>Notas:</h3>
+<p>* El skin solo lo podran ver otras personas, tu te seguirás viendo como un Steve desde tu juego.</p>
+<p>* No se hacen cambios, salvo que se done nuevamente.</p>
+<p>* La personalizacion es por un mes.</p>
+<p>* Nosotros brindaremos la pagina donde podras escoger el skin que te guste.</p>
+
+<div>
+<table class="skinTable" style="text-align: center;display:block;margin-left: 10em;">
+<tbody>
+    <tr>
+        <td>
+            <img src="assets/steve.png" alt="" height="100px"/>
+        </td>
+    </tr>
+
+    <tr>
+        <td> AR$40,00  / u$s 5,00 </td>
+    </tr>
+
+    <tr>
+        <td style="/*padding-left: 3em;box-sizing: border-box*/">
+            <a href="https://www.mercadopago.com/mla/checkout/pay?pref_id=940152-710c87c5-c0a9-4ad1-a7fe-a0a9b5e18163" name="MP-payButton" class="blue-ar-s-rn-arall">Mercado Pago</a>
+            <script type="text/javascript">
+            (function(){function $MPBR_load(){window.$MPBR_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = ("https:"==document.location.protocol?"https://www.mercadopago.com/org-img/jsapi/mptools/buttons/":"http://mp-tools.mlstatic.com/buttons/")+"render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPBR_loaded = true;})();}window.$MPBR_loaded !== true ? $MPBR_load(): null;})();
+            </script>
+        </td>
+
+    </tr>
+
+    <tr>
+        <td style="/*padding-left: 3.5em;box-sizing: border-box*/">
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="DCUXBQ2Y6JPN8">
+            <input type="image" src="http://diquemc.com/images/boton-paypal.png" border="0" name="submit" alt="PayPal. La forma rápida y segura de pagar en Internet.">
+            <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+            </form>
+
+        </td>
+    </tr>
+    </tbody>
+
+</table>
+</div>
+<br/><br/>
+
+<h2>Cuenta Premium:</h2>
+<p>¿Queres tener tu propia cuenta premium?</p>
+<p>
+    Ahora podes adquirir una cuenta OFICIAL PREMIUM DE MINECRAFT y llevarte de regalo un Vip Hierro!
+
+</p>
+<h3>Notas:</h3>
+<p>* Tu puedes elegir el nombre que mas te guste para tu cuenta, pero debe estar disponible .</p>
+<p>* Nosotros nos encargaremos de guiarte paso a paso para la habilitacion de tu cuenta (o la habilitaremos por vos si lo preferis).</p>
+<p>* Tambien nos encargaremos de guiarte paso a paso para que puedas configurar tu skin.</p>
+<p>* El vip de regalo es valido por un mes.</p>
+
+<br>
+<div>
+<table class="premiumTable" style="text-align: center;display:block;margin-left: 10em;">
+<tbody>
+    <tr>
+        <td>
+            <img src="assets/minecraftpremium.png" alt="" height="150px"/>
+        </td>
+    </tr>
+
+    <tr>
+        <td> AR$400,00  </td>
+    </tr>
+
+    <tr>
+        <td style="/*padding-left: 3em;box-sizing: border-box*/">
+            <a href="https://www.mercadopago.com/mla/checkout/pay?pref_id=940152-73c04796-0572-477a-aeb0-5c59a3920e3d" name="MP-payButton" class="blue-ar-s-rn-arall">Mercado Pago</a>
+            <script type="text/javascript">
+            (function(){function $MPBR_load(){window.$MPBR_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = ("https:"==document.location.protocol?"https://www.mercadopago.com/org-img/jsapi/mptools/buttons/":"http://mp-tools.mlstatic.com/buttons/")+"render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPBR_loaded = true;})();}window.$MPBR_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPBR_load) : window.addEventListener('load', $MPBR_load, false)) : null;})();
+            </script>
+        </td>
+
+    </tr>
+
+    <tr>
+        <td style="/*padding-left: 3.5em;box-sizing: border-box*/">
+
+        </td>
+    </tr>
+    </tbody>
+
+</table>
+</div>
+<br/><br/>
+
+
+
+
+<P>
+    <span style="text-decoration: underline">Medios De Pago:</span><br>
+    - MercadoPago: Tarjetas de credito, PagoFacil, RapiPago, Provincia Pagos, Red Link, Banelco. <br/>
+    - PayPal: Pago en dolares. Tarjetas de credito.
+
+</P>
+<p style="color:red">
+    <span style="text-decoration: underline">Nota:</span> Una vez realizada tu compra, contáctate únicamente con un administrador y espera instrucciones.
+</p>
---
+++ assets/res/home.php
@@ -0,0 +1,11 @@
+<?php
+
+require(dirname(__FILE__) . '/../foro/SSI.php');
+header( 'Content-Type:text/html; charset=UTF-8' );
+header( 'Access-Control-Allow-Origin: http://localhost:9292' );
+$result = ssi_fetchPosts(1108,true, 'm') ;
+
+echo "<h2>",$result[0]['subject'],"</h2>";
+echo $result[0]['body'];
+?>
+
